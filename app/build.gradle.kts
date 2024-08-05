0740edbd04ec28fdf3d1ed86f5767b769d92ffa1
ThanhDanh <thanhdanhdo1701@gmail.com>
2024-08-05 17:04:57 +0700

    Setting grandle and Updating activity_main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +46,25 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //lifecycle
+//Thư viện này bao gồm các thành phần lifecycle cơ bản như ViewModel và LiveData.
+//Các thành phần này giúp quản lý vòng đời của các thành phần giao diện người dùng
+// (UI) như Activity và Fragment, giúp đảm bảo dữ liệu và trạng thái của ứng dụng
+// được duy trì đúng cách khi vòng đời của UI thay đổi (ví dụ: khi xoay màn hình).
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
+    //ViewModel
+    implementation("androidx.activity:activity-ktx:1.9.1")
+    // Thư viện Glide cho tải và hiển thị hình ảnh
+    implementation("com.github.bumptech.glide:glide:4.16.0'")
+    // Thư viện DotsIndicator để tạo các dấu chấm chỉ thị trang
+    implementation("com.tbuonomo:dotsindicator:5.0")
 }
