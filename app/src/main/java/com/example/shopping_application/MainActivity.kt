d1dbbab939f3bb37bdcc698f4978a3794f1fb12c
ThanhDanh <thanhdanhdo1701@gmail.com>
2024-08-08 23:17:03 +0700

    Update MainActivity: banner recyclerView

--- app/src/main/java/com/example/shopping_application/MainActivity.kt
+++ app/src/main/java/com/example/shopping_application/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.example.shopping_application
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.example.shopping_application.Model.SliderModel
+import com.example.shopping_application.ViewModel.MainViewModel
+import com.example.shopping_application.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initBanner()
+    }
+
+    private fun initBanner() {
+        //pgbBanner hiển thị lúc đầu, sau khi load data xong ẩn đi
+        binding.pgbBanner.visibility = View.VISIBLE
+        viewModel.banners.observe(this@MainActivity, Observer { items ->
+            banners(items)
+            binding.pgbBanner.visibility = View.GONE
+        })
+        viewModel.loadBanners()
+    }
+
+    private fun banners(images: List<SliderModel>) {
+        binding.vpSlider.adapter = SliderAdapter(images, binding.vpSlider)
+        binding.vpSlider.clipToPadding = false
+        binding.vpSlider.clipChildren = false
+        binding.vpSlider.offscreenPageLimit = 3
+        binding.vpSlider.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.vpSlider.setPageTransformer(compositePageTransformer)
+        if (images.size > 1) {
+            binding.dotIndicator.visibility = View.VISIBLE
+            binding.dotIndicator.attachTo(binding.vpSlider)
         }
     }
 }
---
+++ app/src/main/java/com/example/shopping_application/Model/SliderModel.kt
@@ -0,0 +1,3 @@
+package com.example.shopping_application.Model
+
+data class SliderModel(val url: String = "")
